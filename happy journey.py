d29370878bd3ed7d3b8598760f2a9cff463c8f9d
AkshithaKiramkonda <167959366+AkshithaKiramkonda@users.noreply.github.com>
2024-07-31 13:13:45 +0530

    Add files via upload

---
+++ happy journey.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import time
+import numpy as np
+import pandas as pd
+from PIL import Image as i
+from datetime import datetime,date
+st.set_page_config(page_title="Meet u soonnn",page_icon="❤")
+st.subheader("Happyyyyy Journey raaaaaa potodaaaaaaaaaaaaaaa❤")
+st.image("vphoto/vaishu.jpg")
+st.balloons()
